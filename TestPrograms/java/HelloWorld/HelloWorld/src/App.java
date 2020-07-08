1474cf2472a48753657d544e15c6e39fc3c502c0
Karthicc <37190705+cartha353@users.noreply.github.com>
2020-07-07 20:27:42 -0400

    launch change

--- TestPrograms/java/HelloWorld/HelloWorld/src/App.java
+++ TestPrograms/java/HelloWorld/HelloWorld/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(final String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double myDouble = 6;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
     }
 }
